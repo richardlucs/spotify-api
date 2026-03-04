c67f6a7d122bc0f99205a75870dee352f502d408
Richard Marques <45467028+richardlucs@users.noreply.github.com>
2026-03-04 13:46:34 -0300

    Update spotify.js

--- api/spotify.js
+++ api/spotify.js
@@ -3,13 +3,10 @@ export default async function handler(req, res) {
     const response = await fetch("https://groke.se/twitch/spotify/?254d78a0f93be46993dfd492105dc7ef");
     const text = await response.text();
 
-    res.status(200).json({
-      nowPlaying: text
-    });
+    res.setHeader("Content-Type", "text/plain");
+    res.status(200).send(text);
 
   } catch (error) {
-    res.status(500).json({
-      error: "Erro ao buscar música"
-    });
+    res.status(500).send("Erro ao buscar música");
   }
 }
